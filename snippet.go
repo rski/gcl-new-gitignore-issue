9770b8ebc8f8ef263a9af964a5baa32ca06f89eb
Romanos Skiadas <rski@arista.com>
2018-08-06 18:41:48 +0100

    initial commit

---
+++ snippet.go
@@ -0,0 +1,16 @@
+// -*- mode:go;mode:go-playground -*-
+// snippet of code @ 2018-08-06 18:41:40
+
+// === Go Playground ===
+// Execute the snippet with Ctl-Return
+// Remove the snippet completely with its dir and all files M-x `go-playground-rm`
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Results:")
+}
